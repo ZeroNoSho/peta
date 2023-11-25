dee47e0d273ed3b5ff707d2fbec77a26a592801d
ZeroNoSho <inukuro351@gmail.com>
2023-11-25 08:20:49 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/context/store.js

--- src/context/store.js
+++ src/context/store.js
@@ -1,42 +1,31 @@
 "use client";
-import { useState, useEffect, createContext } from "react";
-import axios from "axios";
+import useSWR from "swr";
+import { useState, createContext } from "react";
 const Contex = createContext(null);
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const Provider = ({ children }) => {
   const [menus, setMenu] = useState("hidden");
-  const [UserDatass, setUserData] = useState([]);
   const [tabel, setTabel] = useState([0, 5]);
 
-  useEffect(() => {
-    getData();
-  }, []);
+  const { data } = useSWR("/api", fetcher);
 
-  //token
-  const getData = async () => {
-    axios
-      .get(`/api`)
-      .then((res) => {
-        const hasil = res.data.message["m2m:list"].map((item, i) => {
-          const { ct, con, rn } = item["m2m:cin"];
-          const jam = ct.substring(9, 11);
-          const menit = ct.substring(11, 13);
-          const obj = JSON.parse(con);
-          const Temperature = obj["Temperature 1"];
-          const Humidity = obj["Humidity 1"];
-          return {
-            no: i,
-            id: rn,
-            year: `${jam}:${menit}`,
-            Temperature,
-            Humidity,
-          };
-        });
-        setUserData(hasil);
-        console.log(hasil);
-      })
-      .catch((err) => {});
-  };
+  const UserDatass = data?.message["m2m:list"].map((item, i) => {
+    const { ct, con, rn } = item["m2m:cin"];
+    const jam = ct.substring(9, 11);
+    const menit = ct.substring(11, 13);
+    const obj = JSON.parse(con);
+    const Temperature = obj["Temperature 1"];
+    const Humidity = obj["Humidity 1"];
+    return {
+      no: i,
+      id: rn,
+      year: `${jam}:${menit}`,
+      Temperature,
+      Humidity,
+    };
+  });
 
   return (
     <Contex.Provider
